021101b423cbd5e3fc5692602ee980ef6519db77
Denys Bezmenov <me@hatomist.pw>
2020-11-28 19:46:49 +0200

    Removed unnecessary include

--- main.h
+++ main.h
@@ -1,8 +1,6 @@
 #ifndef RING_TEST_TASK_AES_MAIN_H
 #define RING_TEST_TASK_AES_MAIN_H
 
-#include <stdint.h>
-
 #define MAX_POS_ARG_NUM 3  // file path, password and out file path
 #define MIN_POS_ARG_NUM 2  // file path and password
 #define HEADER_SIZE 16
